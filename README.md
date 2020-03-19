1d9bb1205f32b5d65d6278027a3ddd771c9267d1
Amin <amin_ghs@yahoo.com>
2020-03-19 22:39:32 +0100

    Updated to Final version

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
-# UdacityLandingpage
-A project for front end nano program: 
+# Landing Page Project
 
-Dynamic Navigation bar with useing javascript
+## Table of Contents
 
+- [Instructions](#instructions)
+- [Development](#development)
+- [ES6](#es6)
+- [Done](#done)
 
-Smoothe scroll when you press on Navigation List
+## Instructions
 
+The starter project has some HTML and CSS styling to display a static version of
+the Landing Page project. You'll need to convert this project from a static
+project to an interactive one. This will require modifying the HTML and CSS
+files, but primarily the JavaScript file.
 
-Useing getBoundingClientRect() for show active section
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the
+Udacity Classroom.
+
+## Development
+
+To run this project locally, in your development folder, run
+`npx http-server -c-1 -o` to run a local development server with no-cache
+enabled and automatically opening your standard browser. Make some changes and
+reload the browser and the changes are applied.
+
+Enjoy!
+
+## ES6
+
+I used several ES6 methods and also Javascript and browser functions which are
+only available in the latest modern browsers, e.g. `IntersectionObserver`.
+Though those are working with the latest browsers except IE 11, IE 10 and Opera
+mini/mobile.
+
+Most of the changes have happened in the `js/app.js` file and some changes for
+styling in `styles.css`
+
+## Done
+
+This project has been enhanced with dynamic generated Navigation list (based on
+the sections) in an unordered list as well as scolling to the section.
--- js/app.js
+++ js/app.js
@@ -1,91 +1,79 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+const navElements = document.querySelectorAll('section')
+const navList = document.getElementById('navbar__list')
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
-*/
+ */
 
+// Build menu by iterating through the navelements
+navElements.forEach(el => {
+  const navlistElement = `<li class='menu__link ${el.className}' data-link=${el.id}><a href="#${el.id}">${el.dataset.nav}</li>`
+  navList.insertAdjacentHTML('beforeend', navlistElement)
+})
 
+// Scroll to section on link click by listenting to the click-event in the navlist
+navList.addEventListener('click', e => {
+  e.preventDefault()
+  const parent = e.target.hasAttribute('data-link')
+    ? e.target
+    : e.target.parentElement
+  const elementToScrollTo = document.getElementById(parent.dataset.link)
+  elementToScrollTo.scrollIntoView({block: 'end', behavior: 'smooth'})
+})
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-function myFunction() {   
-    for (let i = 1; i < 5; i++) {      
-    const li = document.createElement("a");
-    li.appendChild(document.createTextNode("Menu" + i));
-    li.setAttribute("class", "menu__link" );
-    document.getElementById("navbar__list").appendChild(li);
-    li.href = "#section"+i;
-    }
-}
-    myFunction();
-
-
-// Add class 'active' to section when near top of viewport
+// Set section and nav link as active using the IntersectionObserver pattern
 const callback = entries => {
-    entries.forEach(entry => {
-      const navListElement = document.querySelector(
-        `.menu__link[data-link='${entry.target.id}']`,
-      )
-      const section = document.getElementById(entry.target.id)
-  
-      if (entry && entry.isIntersecting) {
-        navListElement.classList.add('active')
-        section.classList.add('active')
-      } else {
-        if (navListElement.classList.contains('active')) {
-          navListElement.classList.remove('active')
-        }
-  
-        if (section.classList.contains('active')) {
-          section.classList.remove('active')
-        }
+  entries.forEach(entry => {
+    const navListElement = document.querySelector(
+      `.menu__link[data-link='${entry.target.id}']`,
+    )
+    const section = document.getElementById(entry.target.id)
+
+    if (entry && entry.isIntersecting) {
+      navListElement.classList.add('active')
+      section.classList.add('active')
+    } else {
+      if (navListElement.classList.contains('active')) {
+        navListElement.classList.remove('active')
       }
-    })
-  }
 
-// Scroll to anchor ID using scrollTO event
+      if (section.classList.contains('active')) {
+        section.classList.remove('active')
+      }
+    }
+  })
+}
 
-const scroll = new SmoothScroll('.navbar__menu a[href*="#"]', {
-    speed: 500
-    });
+// Options for the observer. Most important is the threshold
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.6,
+}
 
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-
-// Scroll to section on link click
-
-// Set sections as active
+// Setting an observer with options and a callback which checks if the navelement should be active
+// support for all modern browser https://caniuse.com/#feat=intersectionobserver
+const observer = new IntersectionObserver(callback, options)
+navElements.forEach(el => {
+  observer.observe(document.getElementById(el.id))
+})
